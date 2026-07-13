514cbab4832240845dd996f27b9d60e790797c2b
Benjamin Yong <benjamin.yong.sg@gmail.com>
2026-07-13 12:36:58 +0800

    Installation of the Tailwind CSS + additional of the delete function to the PO + Invoice Workspace

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -6,6 +6,7 @@ from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls), # if you use default admin
+    path('api/', include('invoices.urls')),
     path('api/health/', health_check),
     path('api/login/', views.obtain_auth_token),
     path('api/invoices/', include('invoices.urls')),
