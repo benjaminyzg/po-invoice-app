dfdc6f0fbd96c2b94cafb2aef4834a335e6f377f
Benjamin Y. <benjamin.yong.sg@gmail.com>
2026-07-14 11:04:05 +0800

    Addition of the requirements.txt file

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -111,6 +111,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
+    "http://localhost:5174",
 ]
 
 REST_FRAMEWORK = {
