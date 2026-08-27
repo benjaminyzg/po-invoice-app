76f732bb6081f6f46941f5793ce201f9a4f748eb
Benjamin Y. <benjamin.yong.sg@gmail.com>
2026-08-27 15:21:38 +0800

    feat(infra): configure production Nginx reverse proxy and Gunicorn WSGI server

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -142,7 +142,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Dynamic Database configuration switcher
 if os.environ.get('DB_ENGINE') == 'django.db.backends.postgresql':
