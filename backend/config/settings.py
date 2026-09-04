158fbeaef93ebb36c92afd7d58e550abd1f6184b
Benjamin Y. <benjamin.yong.sg@gmail.com>
2026-09-04 15:30:41 +0800

    feat: prepare production readiness, fix invoice UI and auth bugs, polish catalog table

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Dynamic Database configuration switcher
 if os.environ.get('DB_ENGINE') == 'django.db.backends.postgresql':
